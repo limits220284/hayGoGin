db070d5ef42edafb41ed60cbbce3cf161eb0e8bf
limits220284 <2056901285@qq.com>
2023-11-18 15:21:46 +0800

    init

---
+++ day1-http-base/base1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "wh Header[%q] = %q\n", k, v)
+	}
+}
---
+++ day1-http-base/base3/go.mod
@@ -0,0 +1,8 @@
+module example
+
+require (
+	gee v0.0.0
+)
+replace (
+	gee => ./gee
+)
