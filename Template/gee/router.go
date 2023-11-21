ee05f71807d53b84084ecc600fb47b491fe8f2aa
limits220284 <2056901285@qq.com>
2023-11-21 15:46:55 +0800

    add Template

---
+++ Template/gee/router.go
@@ -0,0 +1,95 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+//type router struct {
+//	handlers map[string]HandlerFunc
+//}
+
+// roots key eg, roots['GET'] roots['POST']
+// handlers key eg, handlers['GET-/p/:lang/doc'], handlers['POST-/p/book']
+type router struct {
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+func (r *router) handle(c *Context) {
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		c.handlers = append(c.handlers, r.handlers[key])
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+	c.Next()
+}
+
+func (r *router) addRouter(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	key := method + "-" + pattern
+	r.handlers[key] = handler
+}
+
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			//如果第一个字符为*，并且不仅仅是*， 那么记录后面的所有参数即可
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			//如果遇见当前为*的话直接break
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
---
+++ Template/go.mod
@@ -0,0 +1,7 @@
+module Template
+
+go 1.21.3
+
+require gee v0.0.0
+
+replace gee => ./gee
